4f15ab140e47a97267ff925a58f9cd44d6d942d6
diodemusic <diodemusicuk@gmail.com>
2025-11-12 04:40:43 +0000

    Refactor input processing logic in main function

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -7,20 +7,23 @@ fn get_contents(path: &'static str) -> String {
 }
 
 fn main() {
-    // let mut inputs_1: Vec<u32> = Vec::new();
-    // let mut inputs_2: Vec<u32> = Vec::new();
+    let mut inputs_1: Vec<u32> = Vec::new();
+    let mut inputs_2: Vec<u32> = Vec::new();
 
     let path = "src/inputs.txt";
     let contents = get_contents(path);
 
-    for c in contents.chars() {
-        let mut processing = true;
+    let mut assign_to_inputs_1 = true;
 
-        while processing {
-            if c.is_numeric() {
-            } else if c == ' ' {
-                processing = !processing;
+    for c in contents.chars() {
+        if c.is_numeric() {
+            if assign_to_inputs_1 {
+                inputs_1.push(c.to_digit(10).expect("Error parsing"));
+            } else {
+                inputs_2.push(c.to_digit(10).expect("Error parsing"));
             }
+        } else if c == ' ' {
+            assign_to_inputs_1 = !assign_to_inputs_1;
         }
     }
 }

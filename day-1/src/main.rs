88652bdb9322e4ad6a2c796daeed4fba70ab572b
diodemusic <diodemusicuk@gmail.com>
2025-11-12 04:32:54 +0000

    Refactor main function to include file reading logic

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,6 +1,26 @@
-fn main() {
-    let mut inputs_1: Vec<u32> = Vec::new();
-    let mut inputs_2: Vec<u32> = Vec::new();
+use std::fs;
 
-    println!("{:?}", inputs_1);
+fn get_contents(path: &'static str) -> String {
+    let contents = fs::read_to_string(path).expect(&format!("Error reading {}", path));
+
+    contents
+}
+
+fn main() {
+    // let mut inputs_1: Vec<u32> = Vec::new();
+    // let mut inputs_2: Vec<u32> = Vec::new();
+
+    let path = "src/inputs.txt";
+    let contents = get_contents(path);
+
+    for c in contents.chars() {
+        let mut processing = true;
+
+        while processing {
+            if c.is_numeric() {
+            } else if c == ' ' {
+                processing = !processing;
+            }
+        }
+    }
 }
